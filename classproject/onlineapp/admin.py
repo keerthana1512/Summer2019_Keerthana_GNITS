32eb292357b7f44db0e6c8c92f375bddba17c04c
keerthana <keerthanasindoor@gmail.com>
2019-06-09 08:34:49 +0530

    classproject

---
+++ classproject/onlineapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+
+# Register your models0 here.
+admin.site.register(College)
+admin.site.register(Student)
+admin.site.register(MockTest1)
+admin.site.register(Teacher)
