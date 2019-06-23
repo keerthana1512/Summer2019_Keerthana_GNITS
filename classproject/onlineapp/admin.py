320dc81d9a4c48e380275a06e2289610c9248f5e
keerthana <keerthanasindoor@gmail.com>
2019-06-23 14:49:29 +0530

    remove

--- classproject/onlineapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-# Register your models0 here.
-admin.site.register(College)
-admin.site.register(Student)
-admin.site.register(MockTest1)
-admin.site.register(Teacher)
--- classproject/onlineapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OnlineappConfig(AppConfig):
-    name = 'onlineapp'
